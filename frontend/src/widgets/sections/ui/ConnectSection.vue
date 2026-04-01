ceeda6aa729d5808735e025a570c72d8c0fcc756
OstrovskyIv <ostrovskyiml@gmail.com>
2026-04-01 17:28:36 +0300

    Передвинул элементы для блока сетей

--- frontend/src/widgets/sections/ui/ConnectSection.vue
+++ frontend/src/widgets/sections/ui/ConnectSection.vue
@@ -17,24 +17,26 @@ const boostyLink = 'https://boosty.to/last_zone_rp'
 
 <template>
   <section class="w-full h-screen flex flex-col items-center relative snap-start shrink-0 overflow-hidden">
-    <!-- ИЗМЕНЕНО: h-[75px] для мобилок -->
     <div class="w-full h-[75px] sm:h-[90px] md:h-[100px] 2xl:h-[115px] shrink-0 pointer-events-none transition-all duration-500"></div>
 
     <SectionDivider />
 
     <div class="w-full flex-1 flex flex-col items-center justify-center p-4 gap-10 md:gap-20 2xl:gap-32">
+
       <h2 class="text-2xl sm:text-5xl xl:text-7xl font-capture uppercase text-white drop-shadow-[0_0_20px_rgba(255,255,255,0.3)] italic leading-none text-center">
         НАШИ СОЦИАЛЬНЫЕ СЕТИ
       </h2>
 
-      <div class="w-full max-w-7xl flex flex-col gap-8 md:gap-16 items-center">
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 sm:gap-10 w-full max-w-5xl justify-items-center px-2">
+      <div class="w-full max-w-7xl flex flex-col gap-4 sm:gap-10 items-center 2xl:-translate-y-12 transition-transform duration-500">
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-10 w-full max-w-5xl justify-items-center px-2">
           <SocialCard v-for="(social, i) in mainSocials" :key="i" :img="social.img" :link="social.link" />
         </div>
+
         <div class="w-full flex justify-center px-4">
           <SocialCard :img="boostyImg" :link="boostyLink" :isWide="true" />
         </div>
       </div>
+
     </div>
   </section>
 </template>
